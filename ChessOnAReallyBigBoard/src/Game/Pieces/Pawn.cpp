8656af105fd9f9c738f98018276170c34eefedc7
Mihail Kovachev <bionic404.notfound@protonmail.com>
2021-02-19 11:28:47 +0200

    Rewrote pawn movement and fixed a bug where black pawns were always blocked by white pawns and could never block white pawns

--- ChessOnAReallyBigBoard/src/Game/Pieces/Pawn.cpp
+++ ChessOnAReallyBigBoard/src/Game/Pieces/Pawn.cpp
@@ -6,11 +6,13 @@
 Pawn::Pawn(const sf::Sprite& Sprite, EPieceColor Color, Board& nBoard, uint8_t BoardX, uint8_t BoardY)
 	: Piece(Sprite, Color, nBoard, BoardX, BoardY)
 {
+	Direction = m_Color == EPieceColor::Black ? +1 : -1;
 }
 
 Pawn::Pawn(const sf::Texture& Texture, EPieceColor Color, Board& nBoard, uint8_t BoardX, uint8_t BoardY)
 	: Piece(Texture, Color, nBoard, BoardX, BoardY)
 {
+	Direction = m_Color == EPieceColor::Black ? +1 : -1;
 }
 
 bool Pawn::Move(uint8_t NewX, uint8_t  NewY)
@@ -25,47 +27,27 @@ bool Pawn::Move(uint8_t NewX, uint8_t  NewY)
 		return false;
 	}
 
-	EPieceColor CapturedPieceColor;
-
-	bool bCanCapture = false;
-
-	if ((NewX == m_BoardX + 1 || NewX == m_BoardX - 1))
-	{
-		bCanCapture = NewY == m_BoardY + Direction
-			&& m_Board.GetPieceColor(NewX, NewY, CapturedPieceColor) && CapturedPieceColor != m_Color;
-		if (bCanCapture)
-		{
-			m_BoardX = NewX;
-			m_BoardY = NewY;
-			bHasMoved = true;
-			SetReadyToMove(false);
-			return true;
-		}
-	}
-
 	std::array<sf::Vector2u, 2> BlockingSquares;
+
+	EPieceColor CapturedPieceColor;
+	bool bCanCapture = IsBlockedOnDiagonal(0, 0, BlockingSquares);
+	if (bCanCapture &&
+		((NewX == BlockingSquares[0].x && NewY == BlockingSquares[0].y)
+			|| (NewX == BlockingSquares[1].x && NewY == BlockingSquares[1].y)) && 
+		m_Board.GetPieceColor(NewX, NewY, CapturedPieceColor) && CapturedPieceColor != m_Color)
+	{
+		SetReadyToMove(false);
+		m_BoardX = NewX;
+		m_BoardY = NewY;
+		bHasMoved = true;
+		return true;
+	}
+	
 	bool bBlockedOnFile = IsBlockedOnFile(BlockingSquares);
 
-	if (!bHasMoved && bBlockedOnFile && !bCanCapture)
-	{
-		uint8_t DistanceToNewY = std::abs(m_BoardY - NewY);
-
-		if ((BlockingSquares[0].x == m_BoardX && std::abs((int)(BlockingSquares[0].y - m_BoardY)) < DistanceToNewY + 1)
-			||
-			(BlockingSquares[1].x == m_BoardX && std::abs((int)(BlockingSquares[1].y - m_BoardY)) < DistanceToNewY + 1))
-		{
-			SetReadyToMove(false);
-			return false;
-		}
-		else
-		{
-			m_BoardY = NewY;
-			bHasMoved = true;
-			SetReadyToMove(false);
-			return true;
-		}
-	}
-	else if (bCanCapture)
+	uint8_t DistanceToNewY = (uint8_t)std::abs(m_BoardY - NewY);
+	uint8_t DistanceToBlockingPiece = (uint8_t)std::abs(m_BoardY - (uint8_t)BlockingSquares[0].y);
+	if (bBlockedOnFile && DistanceToNewY < DistanceToBlockingPiece)
 	{
 		m_BoardX = NewX;
 		m_BoardY = NewY;
@@ -73,84 +55,64 @@ bool Pawn::Move(uint8_t NewX, uint8_t  NewY)
 		SetReadyToMove(false);
 		return true;
 	}
-	else if (!bHasMoved && !bBlockedOnFile)
+	else if (!bBlockedOnFile)
 	{
-		if (std::abs(NewY - m_BoardY) <= 6 &&
-			NewX == m_BoardX)
+		if (bHasMoved)
 		{
-			m_BoardX = NewX;
-			m_BoardY = NewY;
-			bHasMoved = true;
-			SetReadyToMove(false);
-			return true;
+			if (NewY == m_BoardY + Direction && NewX == m_BoardX)
+			{
+				m_BoardY = NewY;
+				SetReadyToMove(false);
+				return true;
+			}
+			else
+			{
+				SetReadyToMove(false);
+				return false;
+			}
+		}
+		else
+		{
+			if (NewX == m_BoardX && DistanceToNewY < 7)
+			{
+				m_BoardY = NewY;
+				bHasMoved = true;
+				return true;
+			}
 		}
 	}
-	else if (bHasMoved && bBlockedOnFile)
-	{
-		if ((BlockingSquares[0].y == m_BoardY + Direction || BlockingSquares[1].y == m_BoardY + Direction))
-		{
-			SetReadyToMove(false);
-			return false;
-		}
-		else if (NewY == m_BoardY + Direction &&
-			NewX == m_BoardX)
-		{
-			m_BoardY = NewY;
-			SetReadyToMove(false);
-			return true;
-		}
-	}
-	else if (bHasMoved && !bBlockedOnFile)
-	{
-		if (NewY == m_BoardY + Direction &&
-			NewX == m_BoardX)
-		{
-			m_BoardY = NewY;
-			SetReadyToMove(false);
-			return true;
-		}
-	}
-
+	
 	SetReadyToMove(false);
 	return false;
 }
 
 bool Pawn::IsBlockedOnDiagonal(int8_t DiagonalX, int8_t DiagonalY, std::array<sf::Vector2u, 2>& BlockingSquares)
 {
-	return false;
+	uint8_t NumberOfBlockingPieces = 0;
+	if(m_Board.HasPieceAt(m_BoardX + 1, m_BoardY + Direction))
+	{
+		BlockingSquares[0] = sf::Vector2u(m_BoardX + 1, m_BoardY + Direction);
+		++NumberOfBlockingPieces;
+	}
+	if (m_Board.HasPieceAt(m_BoardX - 1, m_BoardY + Direction))
+	{
+		BlockingSquares[1] = sf::Vector2u(m_BoardX - 1, m_BoardY + Direction);
+		++NumberOfBlockingPieces;
+	}
+	return NumberOfBlockingPieces > 0;
 }
 
 bool Pawn::IsBlockedOnFile(std::array<sf::Vector2u, 2>& BlockingSquares)
 {
-	//switch (m_Color)
-	//{
-	//case EPieceColor::White:
-	//	if (Board::IsWithinBoard(m_BoardX, m_BoardY - 1) && m_Board.HasPieceAt(m_BoardX, m_BoardY - 1)) return true;
-	//	break;
-	//case EPieceColor::Black:
-	//	if (Board::IsWithinBoard(m_BoardX, m_BoardY + 1) && m_Board.HasPieceAt(m_BoardX, m_BoardY + 1)) return true;
-	//	break;
-	//}
-
-	int8_t Direction = m_Color == EPieceColor::Black ? +1 : -1;
-
 	if (!bHasMoved)
 	{
-		uint8_t NumBlockingSquares = 0;
-		
 		for (uint8_t y = 1; y <= 6; ++y)
 		{
 			if (m_Board.HasPieceAt(m_BoardX, m_BoardY + Direction * y))
 			{
-				BlockingSquares[NumBlockingSquares++] = sf::Vector2u(m_BoardX, m_BoardY + Direction * y);
-				if (NumBlockingSquares == 2)
-					return true;
+				BlockingSquares[0] = sf::Vector2u(m_BoardX, m_BoardY + Direction * y);
+				return true;
 			}
-		}
-
-		if (NumBlockingSquares > 0)
-		{
-			return true;
 		}
 	}
 	else
@@ -161,7 +123,5 @@ bool Pawn::IsBlockedOnFile(std::array<sf::Vector2u, 2>& BlockingSquares)
 			return true;
 		}
 	}
-
-
 	return false;
 }
--- ChessOnAReallyBigBoard/src/Game/Pieces/Pawn.h
+++ ChessOnAReallyBigBoard/src/Game/Pieces/Pawn.h
@@ -8,10 +8,15 @@ public:
 	Pawn(const sf::Texture& Texture, EPieceColor Color, Board& nBoard, uint8_t BoardX, uint8_t BoardY);
 	virtual bool Move(uint8_t NewX, uint8_t  NewY) override;
 
+	// Diagonal coordinates are ignored for pawns, so they can just be set to 0
 	virtual bool IsBlockedOnDiagonal(int8_t DiagonalX, int8_t DiagonalY, std::array<sf::Vector2u, 2>& BlockingSquares) override;
 	virtual bool IsBlockedOnFile(std::array<sf::Vector2u, 2>& BlockingSquares) override;
 
 private:
+	// Shows the direction in which the pawn can move. Black pawns move down, so their y increases. They have a positive direction;
+	// White pawns move up, so their y decreases. They have a negative direction.
+	int8_t Direction = 0;
+
 	bool bHasMoved = false;
 };
 
