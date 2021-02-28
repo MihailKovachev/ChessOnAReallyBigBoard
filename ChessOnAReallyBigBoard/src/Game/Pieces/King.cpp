0ccf2eea0724a9e3b807286b3efaa0c385175ebc
Mihail Kovachev <bionic404.notfound@protonmail.com>
2021-02-28 18:26:55 +0200

    King movement and capture

--- ChessOnAReallyBigBoard/src/Game/Pieces/King.cpp
+++ ChessOnAReallyBigBoard/src/Game/Pieces/King.cpp
@@ -1,5 +1,7 @@
 #include "King.h"
 
+#include <Game/Board.h>
+
 King::King(const sf::Sprite& Sprite, EPieceColor Color, Board& nBoard, uint8_t BoardX, uint8_t BoardY)
 	: Piece(Sprite, Color, nBoard, BoardX, BoardY)
 {
@@ -12,20 +14,29 @@ King::King(const sf::Texture& Texture, EPieceColor Color, Board& nBoard, uint8_t
 
 bool King::Move(uint8_t NewX, uint8_t  NewY)
 {
+	uint8_t DistanceToNewX = std::abs(NewX - m_BoardX);
+	uint8_t DistanceToNewY = std::abs(NewY - m_BoardY);
+	if (!Board::IsWithinBoard(NewX, NewY) 
+		|| (DistanceToNewX == DistanceToNewY && DistanceToNewX != 1)
+		|| (DistanceToNewX != DistanceToNewY && (DistanceToNewX > 1 || DistanceToNewY > 1))
+		) return false;
+
+	EPieceColor CapturedPieceColor;
+	if (m_Board.HasPieceAt(NewX, NewY) && m_Board.GetPieceColor(NewX, NewY, CapturedPieceColor) && CapturedPieceColor != m_Color)
+	{
+		m_BoardX = NewX;
+		m_BoardY = NewY;
+		bHasMoved = true;
+		return true;
+	}
+	else if (!m_Board.HasPieceAt(NewX, NewY))
+	{
+		m_BoardX = NewX;
+		m_BoardY = NewY;
+		bHasMoved = true;
+		return true;
+	}
+
 	return false;
 }
 
-//bool King::IsBlockedOnDiagonal(int8_t DiagonalX, int8_t DiagonalY)
-//{
-//	return false;
-//}
-
-bool King::IsBlockedOnFile(std::array<sf::Vector2u, 2>& BlockingSquares)
-{
-	return false;
-}
-
-bool King::IsBlockedOnRow(std::array<sf::Vector2u, 2>& BlockingSquares)
-{
-	return false;
-}
