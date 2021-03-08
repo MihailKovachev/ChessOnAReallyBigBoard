b5658e6cb99acedf9bc2e9a67f67e62e8377f58f
Mihail Kovachev <bionic404.notfound@protonmail.com>
2021-03-08 10:56:53 +0200

    Knight movement and capture

--- ChessOnAReallyBigBoard/src/Game/Pieces/Knight.cpp
+++ ChessOnAReallyBigBoard/src/Game/Pieces/Knight.cpp
@@ -1,4 +1,5 @@
 #include "Knight.h"
+#include <Game/Board.h>
 
 Knight::Knight(const sf::Sprite& Sprite, EPieceColor Color, Board& nBoard, uint8_t BoardX, uint8_t BoardY)
 	: Piece(Sprite, Color, nBoard, BoardX, BoardY)
@@ -12,5 +13,23 @@ Knight::Knight(const sf::Texture& Texture, EPieceColor Color, Board& nBoard, uin
 
 bool Knight::Move(uint8_t NewX, uint8_t  NewY)
 {
+	if (!Board::IsWithinBoard(NewX, NewY) || NewX == m_BoardX || NewY == m_BoardY) return false;
+
+	uint8_t DistanceToNewX = std::abs(NewX - m_BoardX);
+	uint8_t DistanceToNewY = std::abs(NewY - m_BoardY);
+
+	if ((DistanceToNewX == 2 && DistanceToNewY == 1) || (DistanceToNewY == 2 && DistanceToNewX == 1))
+	{
+		EPieceColor CapturedPieceColor;
+		m_Board.GetPieceColor(NewX, NewY, CapturedPieceColor);
+		if ((m_Board.GetPieceColor(NewX, NewY, CapturedPieceColor) && CapturedPieceColor != m_Color)
+			|| !m_Board.HasPieceAt(NewX, NewY))
+		{
+			m_BoardX = NewX;
+			m_BoardY = NewY;
+			return true;
+		}
+	}
+
 	return false;
 }
